b04d26622a97c57c63e0c474cdba6b61edd5f735
Aakash malviya <malviyaaakash4509@gmail.com>
2026-01-26 20:20:56 +0530

    fix: the form image not showing and images issue throwing that solve

--- src/appwrite/configur.js
+++ src/appwrite/configur.js
@@ -1,134 +1,136 @@
 import { Client, Storage, Databases, Query, ID } from "appwrite";
 import conf from "../conf/conf";
+
+// Cloud Appwrite Backend Service
 export class Service {
-   client = new Client();
-   bucket;
-   database;
-   constructor() {
-      this.client
-         .setEndpoint(conf.appwriteURL)
-         .setProject(conf.appwriteProjectId);
-      this.database = new Databases(this.client);
-      this.bucket = new Storage(this.client);
-   }
-   async createFeedback({ name, email, feedback }) {
-      try {
-         return await this.database.createDocument(
-            conf.appwriteDatabaseId,
-            conf.appwriteFeedBackId,
-            ID.unique(),
-            {
-               name,
-               email,
-               feedback,
-            }
-         );
-      } catch (error) {
-         throw error
-      }
-   }
-   async createPost({ title, slug, content, featuredImage, status, userId }) {
-      try {
-         return await this.database.createDocument(
-            conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            slug,
-            {
-               title,
-               content,
-               featuredImage,
-               status,
-               userId,
-            }
-         );
-      } catch (error) {
-         throw error;
-      }
-   }
-   async updatePost(slug, { title, content, featuredImage, status }) {
-      try {
-         return await this.database.updateDocument(
-            conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            slug,
-            {
-               title,
-               featuredImage,
-               status,
-               content,
-            }
-         );
-      } catch (error) {
-         throw error;
-      }
-   }
-   async deletePost(slug) {
-      try {
-         return await this.database.deleteDocument(
-            conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            slug
-         );
-      } catch (error) {
-         throw error;
-      }
-   }
-   async listPost() {
-      try {
-         return await this.database.listDocuments(
-            conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            [Query.equal("status", "public")]
-         );
-      } catch (error) {
-         throw error;
-      }
-   }
-   
-   async getPost(slug) {
-      try {
-         return await this.database.getDocument(
-            conf.appwriteDatabaseId,
-            conf.appwriteCollectionId,
-            slug
-         );
-      } catch (error) {
-         throw error;
-      }
-   }
+  client = new Client();
+  bucket;
+  database;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteURL)
+      .setProject(conf.appwriteProjectId);
+    this.database = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+  async createFeedback({ name, email, feedback }) {
+    try {
+      return await this.database.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteFeedBackId,
+        ID.unique(),
+        {
+          name,
+          email,
+          feedback,
+        },
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async createPost({ title, slug, content, featuredImage, status, userId }) {
+    try {
+      return await this.database.createDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        },
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async updatePost(slug, { title, content, featuredImage, status }) {
+    try {
+      return await this.database.updateDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+        {
+          title,
+          featuredImage,
+          status,
+          content,
+        },
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async deletePost(slug) {
+    try {
+      return await this.database.deleteDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async listPost() {
+    try {
+      return await this.database.listDocuments(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        [Query.equal("status", "public")],
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
 
-   async uploadFile(file) {
-      try {
-         return await this.bucket.createFile(
-            conf.appwriteBucketId,
-            ID.unique(),
-            file
-         );
-      } catch (error) {
-         console.log("Appwrite serive :: uploadFile :: error", error);
-         throw error;
-      }
-   }
+  async getPost(slug) {
+    try {
+      return await this.database.getDocument(
+        conf.appwriteDatabaseId,
+        conf.appwriteCollectionId,
+        slug,
+      );
+    } catch (error) {
+      throw error;
+    }
+  }
 
-   async deleteFile(fileId) {
-      try {
-         return await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
-      } catch (error) {
-         throw error;
-      }
-   }
-    getFilePreview(fileId) {
-      try {
-         return  this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
-      } catch (error) {
-         throw error;
-      }
-   }
-   getFileView(fileId) {
-      try {
-         return this.bucket.getFileView(conf.appwriteBucketId, fileId);
-      } catch (error) {}
-   }
+  async uploadFile(file) {
+    try {
+      return await this.bucket.createFile(
+        conf.appwriteBucketId,
+        ID.unique(),
+        file,
+      );
+    } catch (error) {
+      console.log("Appwrite serive :: uploadFile :: error", error);
+      throw error;
+    }
+  }
+
+  async deleteFile(fileId) {
+    try {
+      return await this.bucket.deleteFile(conf.appwriteBucketId, fileId);
+    } catch (error) {
+      throw error;
+    }
+  }
+  getFilePreview(fileId) {
+    try {
+      return this.bucket.getFilePreview(conf.appwriteBucketId, fileId);
+    } catch (error) {
+      throw error;
+    }
+  }
+  getFileView(fileId) {
+    try {
+      return this.bucket.getFileView(conf.appwriteBucketId, fileId);
+    } catch (error) {}
+  }
 }
 
 const service = new Service();
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -8,94 +8,92 @@ import Button from "../components/Button";
 
 import Loading from "./Loading";
 function Post() {
-   const [loading, setLoading] = useState(true);
-   const [posts, setPosts] = useState(null);
-   const { slug } = useParams();
-   const navigate = useNavigate();
-   const userData = useSelector((state) => state.auth.userData);
-   const isAuthor = posts && userData ? posts.userId === userData.$id : false;
-   useEffect(() => {
-      appwriteService
-         .getPost(slug)
-         .then((data) => {
-            if (data) {
-               // console.log(data)
-               setPosts(data);
-               setLoading(false);
-            } else {
-               navigate("/");
-            }
-         })
-         .catch((err) => {
-            console.log(err);
-         });
-   }, [posts]);
-   const deletePost = () => {
-      appwriteService.deletePost(posts.$id).then((status) => {
-         if (status) {
-            appwriteService.deleteFile(posts.featuredImage);
-            navigate("/");
-         }
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState(null);
+  const { slug } = useParams();
+  const navigate = useNavigate();
+  const userData = useSelector((state) => state.auth.userData);
+  const isAuthor = posts && userData ? posts.userId === userData.$id : false;
+  useEffect(() => {
+    appwriteService
+      .getPost(slug)
+      .then((data) => {
+        if (data) {
+          // console.log(data)
+          setPosts(data);
+          setLoading(false);
+        } else {
+          navigate("/");
+        }
+      })
+      .catch((err) => {
+        console.log(err);
       });
-   };
-   return (
-      <div className="py-8 border ">
-         <Container>
-            {loading ? (
-               <Loading />
-            ) : (
-               <div className="grid lg:px-44 justify-items-center ">
-                  <div className="flex justify-center">
-                     <img
-                        src={appwriteService.getFilePreview(
-                           posts.featuredImage
-                        )}
-                        className=" w-80 sm:w-[90%] md:w-[90%]"
-                        alt=""
-                     />
-                  </div>
-                  <div className="w-80 sm:w-[90%] py-10 ">
-                     <h2 className="font-Inter text-xs    sm:text-lg font-bold  text-purple-600 md:text-2xl md:text-left ">
-                        {posts.$createdAt.substring(0, 10)}
-                     </h2>
-                     <title className="font-Inter text-sm sm:text-xl font-bold  md:text-3xl md:pt-2 md:text-left">
-                        {posts.title}
-                     </title>
-                     <article className="  sm:w-full sm:pt-5 font-Inter text-xs sm:text-base pt-2  md:text-left">
-                        {parse(posts.content)}
-                     </article>
-                     {isAuthor ? (
-                        <div className="py-2">
-                           <Link to={`/edit-post/${posts.$id}`}>
-                              <Button
-                                 bgColor="bg-green-200"
-                                 className="mr-3 text-green-700 px-3 py-0.5 sm:px-4 sm:py-0.5 rounded-full text-xs sm:text-base font-medium"
-                              >
-                                 Edit
-                              </Button>
-                           </Link>
-                           <Button
-                              bgColor="bg-red-200"
-                              onClick={deletePost}
-                              textColor="text-red-600"
-                              className="font-medium px-2 py-0.5 rounded-full text-xs sm:text-base "
-                           >
-                              Delete
-                           </Button>
-                        </div>
-                     ) : (
-                        <>
-                           <p className="text-xs text-red-500 pt-4">
-                              Use are not author his post that not show button
-                           </p>
-                        </>
-                     )}
-                  </div>
-               </div>
-            )}
-         </Container>
-      </div>
-   );
+  }, [posts]);
+  const deletePost = () => {
+    appwriteService.deletePost(posts.$id).then((status) => {
+      if (status) {
+        appwriteService.deleteFile(posts.featuredImage);
+        navigate("/");
+      }
+    });
+  };
+  return (
+    <div className="py-8 border ">
+      <Container>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="grid lg:px-44 justify-items-center ">
+            <div className="flex justify-center">
+              <img
+                src={appwriteService.getFileView(posts.featuredImage)}
+                className=" w-80 sm:w-[90%] md:w-[90%]"
+                alt=""
+              />
+            </div>
+            <div className="w-80 sm:w-[90%] py-10 ">
+              <h2 className="font-Inter text-xs    sm:text-lg font-bold  text-purple-600 md:text-2xl md:text-left ">
+                {posts.$createdAt.substring(0, 10)}
+              </h2>
+              <title className="font-Inter text-sm sm:text-xl font-bold  md:text-3xl md:pt-2 md:text-left">
+                {posts.title}
+              </title>
+              <article className="  sm:w-full sm:pt-5 font-Inter text-xs sm:text-base pt-2  md:text-left">
+                {parse(posts.content)}
+              </article>
+              {isAuthor ? (
+                <div className="py-2">
+                  <Link to={`/edit-post/${posts.$id}`}>
+                    <Button
+                      bgColor="bg-green-200"
+                      className="mr-3 text-green-700 px-3 py-0.5 sm:px-4 sm:py-0.5 rounded-full text-xs sm:text-base font-medium"
+                    >
+                      Edit
+                    </Button>
+                  </Link>
+                  <Button
+                    bgColor="bg-red-200"
+                    onClick={deletePost}
+                    textColor="text-red-600"
+                    className="font-medium px-2 py-0.5 rounded-full text-xs sm:text-base "
+                  >
+                    Delete
+                  </Button>
+                </div>
+              ) : (
+                <>
+                  <p className="text-xs text-red-500 pt-4">
+                    Use are not author his post that not show button
+                  </p>
+                </>
+              )}
+            </div>
+          </div>
+        )}
+      </Container>
+    </div>
+  );
 }
 
 export default Post;
